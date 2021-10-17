d34c0dc32c667fa7155dea27ab026f94b92aad4f
BARRY <barry.souleymane88@yahoo.com>
2021-10-17 15:28:24 +0200

    🎉: initial commit

---
+++ src/main/java/com/barry/OpenlabMagasinApplication.java
@@ -0,0 +1,13 @@
+package com.barry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlabMagasinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenlabMagasinApplication.class, args);
+    }
+
+}
